a79273bf2448cb2a4fad7bdb3b6dd5553196593b
Sakthi <sakthiidev@gmail.com>
2024-03-13 16:50:48 +0530

    added file

---
+++ myenemy/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyenemyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myenemy'
---
+++ myenemy/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def user(request):
+    return render(request, 'myfirst.html')
+
+
